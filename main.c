56f17c4673a0e069208870747608d9309ec7e00e
MatteDev02 <62772891+MattDEV02@users.noreply.github.com>
2022-04-02 21:27:12 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,31 @@
 #include "utils/stdutils/main.h"
 #include "utils/positions/main.h"  
 #include "utils/pieces/main.h"   
-#include "utils/field/main.h"
+#include "utils/field/main.h"   
 
 
 int main(void) {
     unsigned short 
-        movesCounter = 0,
         player = 0,
         winner = 0;
+    static unsigned short movesCounter = 0;
     bool win = false;
     Field field = fieldCalloc();
     initField(field);
 	while((player < 2) && (!win && movesCounter < 9)) {
-		win = playerTurn(field, player, &(movesCounter));
+		win = playerTurn(field, player, movesCounter);
+        movesCounter++;
         winner = player + 1;
 		if(player == 0)
             player++;
         else 
             player = 0;
-	}
+	} 
     printField(field);
     fieldDealloc(field);
 	if(movesCounter >= 9 && !win)   // condizione di pareggio
 		printf("\n \nPareggio tra giocatore 1 e giocatore 2.\n \n");
 	else if(win)   
-		printf("\n \nHa vinto il giocatore numero %i in %i mosse.\n \n", winner, movesCounter);
+		printf("\n \nHa vinto il giocatore numero %i (%c) in %i mosse.\n \n", winner, getPiece(winner - 1) , movesCounter);
 	return EXIT_SUCCESS;   
 }
